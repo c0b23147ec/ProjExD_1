9d138b0a9d0647f8793e6129d14e068912f108d2
yoshioka shuto <c0b23147ec@edu.teu.ac.jp>
2024-11-19 17:13:03 +0900

    演習2

--- pygame_image.py
+++ pygame_image.py
@@ -16,8 +16,6 @@ def main():
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
     tmr = 0
-
-
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
@@ -28,18 +26,22 @@ def main():
         screen.blit(bg_img, [x+3200, 0])
         screen.blit(bg_img2, [x + 4800, 0])
 
-        
+        dis = 0
+        y = 0
+
         key_lst = pg.key.get_pressed()
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0, -1))
+            y += -1
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0, 1))
+            y += 1
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1,0))
+            dis += -1
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((1, 0))
+            dis += 1
         else:
-            kk_rct.move_ip(-1, 0)
+            dis +=-1
+
+        kk_rct.move_ip((dis, y))
 
         screen.blit(kk_img, kk_rct) #こうかとんおを300, 200の位置に描画
         pg.display.update()
