e59d959fd59c804432b5f83befc3c91a2c39d94c
yoshioka shuto <c0b23147ec@edu.teu.ac.jp>
2024-11-19 15:48:24 +0900

    練習6-1:背景を移動させる

--- pygame_image.py
+++ pygame_image.py
@@ -10,18 +10,18 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    kk_img = pg.image.load("fig/3.png")
-    kk_img = pg.transform.flip(kk_img, True, False)
+    kk_img = pg.image.load("fig/3.png")    #こうかとん画像の読み込み
+    kk_img = pg.transform.flip(kk_img, True, False) #こうかとん画像を反転
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [300,200])
+        screen.blit(bg_img, [-tmr, 0])
+        screen.blit(kk_img, [300,200]) #こうかとんおを300, 200の位置に描画
         pg.display.update()
         tmr += 1        
-        clock.tick(200)
+        clock.tick(200) #FPSを200に変更
 
 
 if __name__ == "__main__":
