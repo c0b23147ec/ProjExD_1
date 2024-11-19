c74d4cb80d9394d49db8a48eafb338e835d1cb7b
yoshioka shuto <c0b23147ec@edu.teu.ac.jp>
2024-11-19 17:00:08 +0900

    演習1

--- pygame_image.py
+++ pygame_image.py
@@ -16,26 +16,30 @@ def main():
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
     tmr = 0
+
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        
-        key_lst = pg.key.get_pressed()
-        if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0, -1))
-        elif key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0, 1))
-        elif key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1,0))
-        elif key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((1, 0))
-
 
         x = -(tmr % 3200)
         screen.blit(bg_img, [x,0])
         screen.blit(bg_img2, [x + 1600, 0])
         screen.blit(bg_img, [x+3200, 0])
         screen.blit(bg_img2, [x + 4800, 0])
+
+        
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip((0, -1))
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((0, 1))
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-1,0))
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((1, 0))
+        else:
+            kk_rct.move_ip(-1, 0)
 
         screen.blit(kk_img, kk_rct) #こうかとんおを300, 200の位置に描画
         pg.display.update()
