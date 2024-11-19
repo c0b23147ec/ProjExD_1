6f94cc85fc2d0bf2f4ae17ff45cfea57c6bf369e
yoshioka shuto <c0b23147ec@edu.teu.ac.jp>
2024-11-19 15:56:05 +0900

    練習6-2:背景の残像をなくす

--- pygame_image.py
+++ pygame_image.py
@@ -16,8 +16,8 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [-tmr, 0])
+        x = tmr % 800
+        screen.blit(bg_img, [-x, 0])
         screen.blit(kk_img, [300,200]) #こうかとんおを300, 200の位置に描画
         pg.display.update()
         tmr += 1        
